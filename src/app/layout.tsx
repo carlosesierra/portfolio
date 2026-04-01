e819d3cfcaa773f3cfd67f447160de6f64f60f27
carlosesierra <carlosesierra@users.noreply.github.com>
2026-04-01 16:29:11 +1100

    feat: update Google Tag Manager ID for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,12 +54,12 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','G-XNH4ELNVVN');`}
+          })(window,document,'script','dataLayer','GTM-5N5FGPR');`}
         </Script>
 
         <noscript>
           <iframe
-            src='https://www.googletagmanager.com/ns.html?id=G-XNH4ELNVVN'
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5N5FGPR'
             height='0'
             width='0'
             style={{ display: 'none', visibility: 'hidden' }}
